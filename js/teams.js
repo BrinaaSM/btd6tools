84992f7d7f767f415cfe96df9f23e9a935345097
BrinaaSM (Sabrina) <151764173+BrinaaSM@users.noreply.github.com>
2025-05-28 17:51:22 +0200

    chimps list thinning

--- js/teams.js
+++ js/teams.js
@@ -8,69 +8,70 @@ class Category {
 }
 
 class Tower {
-	constructor(name, category, checkBoxElem, labelElem, isHero) {
+	constructor(name, category, checkBoxElem, labelElem, isHero, isChimpsStart) {
 		this.name = name;
 		this.category = category;
 		this.box = checkBoxElem;
 		this.label = labelElem;
 		this.hero = isHero;
+		this.start = isChimpsStart;
 	}
 }
 
 // create hero classes
-const anyHero = new Tower("Any Hero", "hero", document.getElementById("any-hero"), document.getElementById("any-hero-label"), true);
-const quincy = new Tower("Quincy", "hero", document.getElementById("quincy"), document.getElementById("quincy-label"), true);
-const gwen = new Tower("Gwendolyn", "hero", document.getElementById("gwen"), document.getElementById("gwen-label"), true);
-const striker = new Tower("Striker Jones", "hero", document.getElementById("striker"), document.getElementById("striker-label"), true);
-const obyn = new Tower("Obyn Greenfoot", "hero", document.getElementById("obyn"), document.getElementById("obyn-label"), true);
-const churchill = new Tower("Captain Churchill", "hero", document.getElementById("churchill"), document.getElementById("churchill-label"), true);
-const ben = new Tower("Benjamin", "hero", document.getElementById("ben"), document.getElementById("ben-label"), true);
-const ezili = new Tower("Ezili", "hero", document.getElementById("ezili"), document.getElementById("ezili-label"), true);
-const pat = new Tower("Pat Fusty", "hero", document.getElementById("pat"), document.getElementById("pat-label"), true);
-const adora = new Tower("Adora", "hero", document.getElementById("adora"), document.getElementById("adora-label"), true);
-const brickell = new Tower("Admiral Brickell", "hero", document.getElementById("brickell"), document.getElementById("brickell-label"), true);
-const etienne = new Tower("Etienne", "hero", document.getElementById("etienne"), document.getElementById("etienne-label"), true);
-const sauda = new Tower("Sauda", "hero", document.getElementById("sauda"), document.getElementById("sauda-label"), true);
-const psi = new Tower("Psi", "hero", document.getElementById("psi"), document.getElementById("psi-label"), true);
-const gerry = new Tower("Geraldo", "hero", document.getElementById("gerry"), document.getElementById("gerry-label"), true);
-const corvus = new Tower("Corvus", "hero", document.getElementById("corvus"), document.getElementById("corvus-label"), true);
-const rosalia = new Tower("Rosalia", "hero", document.getElementById("rosalia"), document.getElementById("rosalia-label"), true);
+const anyHero = new Tower("Any Hero", "hero", document.getElementById("any-hero"), document.getElementById("any-hero-label"), true, true);
+const quincy = new Tower("Quincy", "hero", document.getElementById("quincy"), document.getElementById("quincy-label"), true, true);
+const gwen = new Tower("Gwendolyn", "hero", document.getElementById("gwen"), document.getElementById("gwen-label"), true, false);
+const striker = new Tower("Striker Jones", "hero", document.getElementById("striker"), document.getElementById("striker-label"), true, false);
+const obyn = new Tower("Obyn Greenfoot", "hero", document.getElementById("obyn"), document.getElementById("obyn-label"), true, false);
+const churchill = new Tower("Captain Churchill", "hero", document.getElementById("churchill"), document.getElementById("churchill-label"), true, false);
+const ben = new Tower("Benjamin", "hero", document.getElementById("ben"), document.getElementById("ben-label"), true, false);
+const ezili = new Tower("Ezili", "hero", document.getElementById("ezili"), document.getElementById("ezili-label"), true, true);
+const pat = new Tower("Pat Fusty", "hero", document.getElementById("pat"), document.getElementById("pat-label"), true, false);
+const adora = new Tower("Adora", "hero", document.getElementById("adora"), document.getElementById("adora-label"), true, false);
+const brickell = new Tower("Admiral Brickell", "hero", document.getElementById("brickell"), document.getElementById("brickell-label"), true, false);
+const etienne = new Tower("Etienne", "hero", document.getElementById("etienne"), document.getElementById("etienne-label"), true, false);
+const sauda = new Tower("Sauda", "hero", document.getElementById("sauda"), document.getElementById("sauda-label"), true, true);
+const psi = new Tower("Psi", "hero", document.getElementById("psi"), document.getElementById("psi-label"), true, false);
+const gerry = new Tower("Geraldo", "hero", document.getElementById("gerry"), document.getElementById("gerry-label"), true, false);
+const corvus = new Tower("Corvus", "hero", document.getElementById("corvus"), document.getElementById("corvus-label"), true, false);
+const rosalia = new Tower("Rosalia", "hero", document.getElementById("rosalia"), document.getElementById("rosalia-label"), true, false);
 
 // create primary classes
-const anyPrimary = new Tower("Any Primary Monkey", "primary", document.getElementById("any-primary"), document.getElementById("any-primary-label"), false);
-const dart = new Tower("Dart Monkey", "primary", document.getElementById("dart"), document.getElementById("dart-label"), false);
-const boomerang = new Tower("Boomerang Monkey", "primary", document.getElementById("boomerang"), document.getElementById("boomerang-label"), false);
-const bomb = new Tower("Bomb Shooter", "primary", document.getElementById("bomb"), document.getElementById("bomb-label"), false);
-const tack = new Tower("Tack Shooter", "primary", document.getElementById("tack"), document.getElementById("tack-label"), false);
-const ice = new Tower("Ice Monkey", "primary", document.getElementById("ice"), document.getElementById("ice-label"), false);
-const glue = new Tower("Glue Gunner", "primary", document.getElementById("glue"), document.getElementById("glue-label"), false);
+const anyPrimary = new Tower("Any Primary Monkey", "primary", document.getElementById("any-primary"), document.getElementById("any-primary-label"), false, true);
+const dart = new Tower("Dart Monkey", "primary", document.getElementById("dart"), document.getElementById("dart-label"), false, true);
+const boomerang = new Tower("Boomerang Monkey", "primary", document.getElementById("boomerang"), document.getElementById("boomerang-label"), false, true);
+const bomb = new Tower("Bomb Shooter", "primary", document.getElementById("bomb"), document.getElementById("bomb-label"), false, false);
+const tack = new Tower("Tack Shooter", "primary", document.getElementById("tack"), document.getElementById("tack-label"), false, true);
+const ice = new Tower("Ice Monkey", "primary", document.getElementById("ice"), document.getElementById("ice-label"), false, false);
+const glue = new Tower("Glue Gunner", "primary", document.getElementById("glue"), document.getElementById("glue-label"), false, false);
 
 // create military classes
-const anyMilitary = new Tower("Any Military Monkey", "military", document.getElementById("any-military"), document.getElementById("any-military-label"), false);
-const sniper = new Tower("Sniper Monkey", "military", document.getElementById("sniper"), document.getElementById("sniper-label"), false);
-const sub = new Tower("Monkey Sub", "military", document.getElementById("sub"), document.getElementById("sub-label"), false);
-const bucc = new Tower("Monkey Buccaneer", "military", document.getElementById("bucc"), document.getElementById("bucc-label"), false);
-const ace = new Tower("Monkey Ace", "military", document.getElementById("ace"), document.getElementById("ace-label"), false);
-const heli = new Tower("Heli Pilot", "military", document.getElementById("heli"), document.getElementById("heli-label"), false);
-const mortar = new Tower("Mortar Monkey", "military", document.getElementById("mortar"), document.getElementById("mortar-label"), false);
-const dartling = new Tower("Dartling Gunner", "military", document.getElementById("dartling"), document.getElementById("dartling-label"), false);
+const anyMilitary = new Tower("Any Military Monkey", "military", document.getElementById("any-military"), document.getElementById("any-military-label"), false, true);
+const sniper = new Tower("Sniper Monkey", "military", document.getElementById("sniper"), document.getElementById("sniper-label"), false, false);
+const sub = new Tower("Monkey Sub", "military", document.getElementById("sub"), document.getElementById("sub-label"), false, true);
+const bucc = new Tower("Monkey Buccaneer", "military", document.getElementById("bucc"), document.getElementById("bucc-label"), false, true);
+const ace = new Tower("Monkey Ace", "military", document.getElementById("ace"), document.getElementById("ace-label"), false, false);
+const heli = new Tower("Heli Pilot", "military", document.getElementById("heli"), document.getElementById("heli-label"), false, false);
+const mortar = new Tower("Mortar Monkey", "military", document.getElementById("mortar"), document.getElementById("mortar-label"), false, false);
+const dartling = new Tower("Dartling Gunner", "military", document.getElementById("dartling"), document.getElementById("dartling-label"), false, false);
 
 // create magic classes
-const anyMagic = new Tower("Any Magic Monkey", "magic", document.getElementById("any-magic"), document.getElementById("any-magic-label"), false);
-const wiz = new Tower("Wizard Monkey", "magic", document.getElementById("wiz"), document.getElementById("wiz-label"), false);
-const superm = new Tower("Supermonkey", "magic", document.getElementById("superm"), document.getElementById("superm-label"), false);
-const ninja = new Tower("Ninja Monkey", "magic", document.getElementById("ninja"), document.getElementById("ninja-label"), false);
-const alch = new Tower("Alchemist", "magic", document.getElementById("alch"), document.getElementById("alch-label"), false);
-const druid = new Tower("Druid", "magic", document.getElementById("druid"), document.getElementById("druid-label"), false);
-const mermonkey = new Tower("Mermonkey", "magic", document.getElementById("mermonkey"), document.getElementById("mermonkey-label"), false);
+const anyMagic = new Tower("Any Magic Monkey", "magic", document.getElementById("any-magic"), document.getElementById("any-magic-label"), false, true);
+const wiz = new Tower("Wizard Monkey", "magic", document.getElementById("wiz"), document.getElementById("wiz-label"), false, true);
+const superm = new Tower("Supermonkey", "magic", document.getElementById("superm"), document.getElementById("superm-label"), false, false);
+const ninja = new Tower("Ninja Monkey", "magic", document.getElementById("ninja"), document.getElementById("ninja-label"), false, true);
+const alch = new Tower("Alchemist", "magic", document.getElementById("alch"), document.getElementById("alch-label"), false, false);
+const druid = new Tower("Druid", "magic", document.getElementById("druid"), document.getElementById("druid-label"), false, true);
+const mermonkey = new Tower("Mermonkey", "magic", document.getElementById("mermonkey"), document.getElementById("mermonkey-label"), false, true);
 
 // create support classes
-const anySupport = new Tower("Any Support Monkey", "support", document.getElementById("any-support"), document.getElementById("any-support-label"), false);
-const farm = new Tower("Banana Farm", "support", document.getElementById("farm"), document.getElementById("farm-label"), false);
-const spac = new Tower("Spike Factory", "support", document.getElementById("spac"), document.getElementById("spac-label"), false);
-const village = new Tower("Monkey Village", "support", document.getElementById("village"), document.getElementById("village-label"), false);
-const engi = new Tower("Engineer Monkey", "support", document.getElementById("engi"), document.getElementById("engi-label"), false);
-const beast = new Tower("Beast Handler", "support", document.getElementById("beast"), document.getElementById("beast-label"), false);
+const anySupport = new Tower("Any Support Monkey", "support", document.getElementById("any-support"), document.getElementById("any-support-label"), false, true);
+const farm = new Tower("Banana Farm", "support", document.getElementById("farm"), document.getElementById("farm-label"), false, false);
+const spac = new Tower("Spike Factory", "support", document.getElementById("spac"), document.getElementById("spac-label"), false, false);
+const village = new Tower("Monkey Village", "support", document.getElementById("village"), document.getElementById("village-label"), false, false);
+const engi = new Tower("Engineer Monkey", "support", document.getElementById("engi"), document.getElementById("engi-label"), false, true);
+const beast = new Tower("Beast Handler", "support", document.getElementById("beast"), document.getElementById("beast-label"), false, true);
 
 const sizeInput = document.getElementById("team-size-input");
 const teamOutput = document.getElementById("team-output");
@@ -96,20 +97,29 @@ const chosenTowers = [];
 const heroList = [];
 const primaryList = [];
 const militaryList = [];
-const magicList= [];
-const supportList= [];
+const magicList = [];
+const supportList = [];
 
-var heroCat = true;
-var primaryCat = true;
-var militaryCat = true;
-var magicCat = true;
-var supportCat = true;
+const categoryList = [];
 
-const hero = new Category("Heroes", heroList, document.getElementById("hero"), heroCat);
-const primary = new Category("Primary", primaryList, document.getElementById("primary"), primaryCat);
-const military = new Category("Military", militaryList, document.getElementById("military"), militaryCat);
-const magic = new Category("Magic", magicList, document.getElementById("magic"), magicCat);
-const support = new Category("Support", supportList, document.getElementById("support"), supportCat);
+var hero = true;
+var primary = true;
+var military = true;
+var magic = true;
+var support = true;
+
+const heroCat = new Category("Heroes", heroList, document.getElementById("hero"), hero);
+const primaryCat = new Category("Primary", primaryList, document.getElementById("primary"), primary);
+const militaryCat = new Category("Military", militaryList, document.getElementById("military"), military);
+const magicCat = new Category("Magic", magicList, document.getElementById("magic"), magic);
+const supportCat = new Category("Support", supportList, document.getElementById("support"), support);
+
+categoryList.push(heroCat);
+categoryList.push(primaryCat);
+categoryList.push(militaryCat);
+categoryList.push(magicCat);
+categoryList.push(supportCat);
+
 
 function fillCategoryLists() {
 	heroList.push(anyHero);
@@ -171,7 +181,6 @@ function initPage() {
 	for(let i = 1; i < heroList.length; i++) {
 		heroList[i].box.checked = false;
 	}
-	heroCat = false;
 	primaryList[0].box.checked = false;
 	militaryList[0].box.checked = false;
 	magicList[0].box.checked = false;
@@ -227,138 +236,22 @@ function toggleCategory(category) {
 	return;
 }
 
-/*function toggleHero() {
-	for(let i = 0; i < heroList.length; i++) {
-		heroList[i].box.checked = heroCat;
-		heroList[i].box.disabled = !heroCat;
-	}
-	if(hero) {
-		anyHero.box.checked = false;
-	}
-	
-	return;
-}
-
-function togglePrimary() {
-	for(let i = 0; i < primaryList.length; i++) {
-		primaryList[i].box.checked = primaryCat;
-		primaryList[i].box.disabled = !primaryCat;
-	}
-	if(primary) {
-		anyPrimary.box.checked = false;
-	}
-	
-	return;
-}
-
-function toggleMilitary() {
-	for(let i = 0; i < militaryList.length; i++) {
-		militaryList[i].box.checked = militaryCat;
-		militaryList[i].box.disabled = !militaryCat;
-	}
-	if(military) {
-		anyMilitary.box.checked = false;
-	}
-	
-	return;
-}
-
-function toggleMagic() {
-	for(let i = 0; i < magicList.length; i++) {
-		magicList[i].box.checked = magicCat;
-		magicList[i].box.disabled = !magicCat;
-	}
-	if(magic) {
-		anyMagic.box.checked = false;
-	}
-	
-	return;
-}
-
-function toggleSupport() {
-	for(let i = 0; i < supportList.length; i++) {
-		supportList[i].box.checked = supportCat;
-		supportList[i].box.disabled = !supportCat;
-	}
-	if(support) {
-		anySupport.box.checked = false;
-	}
-	
-	return;
-}
-*/
-
 // fill list with towers that can start C.H.I.M.P.S.
 function fillChimpsLists() {
 	chimpsViableTowers.length = 0;
 	chimpsViableHeroes.length = 0;
 	chimpsCount = 0;
 	
-	// viable heroes	
-	if(anyHero.box.checked) {
-		chimpsViableTowers.push(anyHero);
-		chimpsViableHeroes.push(anyHero);
-		chimpsCount++;
-	}
-	if(quincy.box.checked) {
-		chimpsViableTowers.push(quincy);
-		chimpsViableHeroes.push(quincy);
-		chimpsCount++;
-	}
-	if(sauda.box.checked) {
-		chimpsViableTowers.push(sauda);
-		chimpsViableHeroes.push(sauda);
-		chimpsCount++;
-	}
-	
-	// viable towers
-	if(anyPrimary.box.checked) {
-		chimpsViableTowers.push(anyPrimary);
-		chimpsCount++;
-	}
-	if(dart.box.checked) {
-		chimpsViableTowers.push(dart);
-		chimpsCount++;
-	}
-	if(boomerang.box.checked) {
-		chimpsViableTowers.push(boomerang);
-		chimpsCount++;
-	}
-	if(anyMilitary.box.checked) {
-		chimpsViableTowers.push(anyMilitary);
-		chimpsCount++;
-	}
-	if(sub.box.checked) {
-		chimpsViableTowers.push(sub);
-		chimpsCount++;
-	}
-	if(anyMagic.box.checked) {
-		chimpsViableTowers.push(anyMagic);
-		chimpsCount++;
-	}
-	if(wiz.box.checked) {
-		chimpsViableTowers.push(wiz);
-		chimpsCount++;
-	}
-	if(ninja.box.checked) {
-		chimpsViableTowers.push(ninja);
-		chimpsCount++;
-	}
-	if(druid.box.checked) {
-		chimpsViableTowers.push(druid);
-		chimpsCount++;
-	}
-	if(mermonkey.box.checked) {
-		chimpsViableTowers.push(mermonkey);
-		chimpsCount++;
-	}
-	if(anySupport.box.checked) {
-		chimpsViableTowers.push(anySupport);
-		chimpsCount++;
-	}
-	if(engi.box.checked) {
-		chimpsViableTowers.push(engi);
-		chimpsCount++;
+	for(let i = 0; i < categoryList.length; i++) {
+		for(let j = 0; j < categoryList[i].list.length; j++) {
+			if (categoryList[i].list[j].box.checked && categoryList[i].list[j].start) {
+				chimpsViableTowers.push(categoryList[i].list[j]);
+				chimpsCount++;
+				if (categoryList[i].list[j].hero) {
+					chimpsViableHeroes.push(categoryList[i].list[j]);
+				}
+			}
+		}
 	}
 	
 	return;
